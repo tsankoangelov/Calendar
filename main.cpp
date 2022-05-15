b29e7440d80a41e4737eec82d18dc10d019e75f7
tsankoangelov <63124507+tsankoangelov@users.noreply.github.com>
2022-05-15 19:15:18 +0300

    Update main.cpp
    Improve main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ void CommandPromt()
         if (strcmp(word, "EXIT") == 0)
         {
             std::cout << "Have a nice day!" << std::endl;
+            cal.export_to_file();
             return;
         }
         else if (strcmp(word, "ADD") == 0)
@@ -28,10 +29,15 @@ void CommandPromt()
         }
         else if (strcmp(word, "REMOVE") == 0)
         {
-            std::cout << "Please type the information about the event you want to remove!(name, comment, begin and end date)" << std::endl;
-            Event e;
-            std::cin >> e;
-            cal.remove(e);
+            std::cout << "Please type the information about the event you want to remove!(name and begin date)" << std::endl;
+            char name[100];
+            do
+            {
+                std::cin.getline(name, 100);
+            } while (strlen(name) > 100);
+            Date d;
+            std::cin >> d;
+            cal.remove(name, d);
             std::cin.ignore();
         }
         else if (strcmp(word, "DAILY EVENTS") == 0)
@@ -44,10 +50,16 @@ void CommandPromt()
         }
         else if (strcmp(word, "EDIT") == 0)
         {
-            std::cout << "Please type the information about the event you want to edit!(name, comment, begin and end date)" << std::endl;
-            Event e;
-            std::cin >> e;
-            cal.edit(e);
+            std::cout << "Please type the information about the event you want to edit!(name and begin date)" << std::endl;
+            char name[100];
+            do
+            {
+                std::cin.getline(name, 100);
+            } while (strlen(name) > 100);
+            Date d;
+            std::cin >> d;
+            std::cin.ignore();
+            cal.edit(name, d);
             std::cin.ignore();
         }
         else if (strcmp(word, "SEARCH") == 0)
@@ -72,7 +84,7 @@ void CommandPromt()
         }
         else if (strcmp(word, "FREE SLOT") == 0)
         {
-            std::cout << "Find free slot for given period for your event" << std::endl;
+            std::cout << "Find free slot between 8:00 and 19:00 in desired period" << std::endl;
             std::cout << "Please enter begin date of the period" << std::endl;
             Date begin;
             std::cin >> begin;
